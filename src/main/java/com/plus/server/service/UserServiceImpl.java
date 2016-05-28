bcf32d69bd65b2ce77b22f4a2b5725291573518e
chuly <chuly@chuly-PC>
2016-05-28 18:04:32 +0800

    1111

--- src/main/java/com/plus/server/service/UserServiceImpl.java
+++ src/main/java/com/plus/server/service/UserServiceImpl.java
@@ -1,74 +1,67 @@
 package com.plus.server.service;
 
-import com.plus.server.dal.UserDao;
-import com.plus.server.model.User;
-import org.springframework.stereotype.Service;
-import sun.misc.BASE64Encoder;
-
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
+
+import org.springframework.stereotype.Service;
+
+import com.plus.server.dal.UserDAO;
+import com.plus.server.model.User;
 
 /**
  * Created by jiangwulin on 16/5/22.
  */
 @Service("userService")
 public class UserServiceImpl implements UserService {
-    UserDao userDao;
+	UserDAO userDao;
 
-    public boolean register(String phone, String email, String password) {
-        User user = new User(phone,email);
-        user.setValid(1);
-        String password_salt = generateSalt(5);
-        String password_hash = getPasswordHash(password, password_salt);
-        user.setPassword_salt(password_salt);
-        user.setPassword_hash(password_hash);
+	public boolean register(String phone, String email, String password) {
+		User user = new User();
+		// user.setValid(1);
+		// String password_salt = generateSalt(5);
+		// String password_hash = getPasswordHash(password, password_salt);
+		// user.setPassword_salt(password_salt);
+		// user.setPassword_hash(password_hash);
 
-        return userDao.insert(user);
-    }
+		userDao.insert(user);
+		return true;
+	}
 
-    public boolean login(User user) {
-        return false;
-    }
+	public boolean login(User user) {
+		return false;
+	}
 
+	private static String generateSalt(int size) {
+		byte[] saltBytes = new byte[size];
+		SecureRandom secureRandom = new SecureRandom();
+		secureRandom.nextBytes(saltBytes);
 
+		return Base64.getEncoder().encodeToString(saltBytes);
+	}
 
-    private static String generateSalt(int size)
-    {
-        byte[] saltBytes = new byte[size];
-        SecureRandom secureRandom = new SecureRandom();
-        secureRandom.nextBytes(saltBytes);
+	private static String getPasswordHash(String password, String salt) {
+		String strResult = "";
+		try {
+			MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
+			messageDigest.update((password + salt).getBytes());
+			byte[] byteBuffer = messageDigest.digest();
+			StringBuffer strHexString = new StringBuffer();
 
-        return Base64.getEncoder().encodeToString(saltBytes);
-    }
+			for (int i = 0; i < byteBuffer.length; i++) {
+				String hex = Integer.toHexString(0xff & byteBuffer[i]);
+				if (hex.length() == 1) {
+					strHexString.append('0');
+				}
+				strHexString.append(hex);
+			}
 
-    private static String getPasswordHash(String password, String salt)
-    {
-        String strResult = "";
-        try {
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
-            messageDigest.update((password + salt).getBytes());
-            byte[] byteBuffer = messageDigest.digest();
-            StringBuffer strHexString = new StringBuffer();
+			strResult = strHexString.toString();
 
-            for (int i = 0; i < byteBuffer.length; i++)
-            {
-                String hex = Integer.toHexString(0xff & byteBuffer[i]);
-                if (hex.length() == 1)
-                {
-                    strHexString.append('0');
-                }
-                strHexString.append(hex);
-            }
-
-            strResult = strHexString.toString();
-
-        }
-        catch (NoSuchAlgorithmException ex)
-        {
-            //TODO:log
-        }
-        return strResult.substring(0, 66);
-    }
+		} catch (NoSuchAlgorithmException ex) {
+			// TODO:log
+		}
+		return strResult.substring(0, 66);
+	}
 }

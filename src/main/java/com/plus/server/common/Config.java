cd0468b7a244baea198117114d7705d338f8d751
chuly <chuly@chuly-PC>
2016-05-28 17:57:36 +0800

    000

---
+++ src/main/java/com/plus/server/common/Config.java
@@ -0,0 +1,13 @@
+package com.plus.server.common;
+
+public class Config {
+	/*
+	 '付款最长等待时间(分钟)',
+  	'单次最大购买数',	
+  	能存在的最多未付款订单数
+  	RMB兑美元汇率
+  	参与人数系数a（公式：x*a+b）
+  	参与人数系数b（公式：x*a+b）
+	 */
+
+}

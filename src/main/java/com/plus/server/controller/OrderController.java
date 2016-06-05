7caa92b6bf75d9aca2598473760375f23806fbb9
chuly <chuly@chuly-PC>
2016-06-05 16:50:37 +0800

    909099909

--- src/main/java/com/plus/server/controller/OrderController.java
+++ src/main/java/com/plus/server/controller/OrderController.java
@@ -1,14 +1,22 @@
 package com.plus.server.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.plus.server.service.OrderService;
+import com.wordnik.swagger.annotations.Api;
 
-/**
- * Created by jiangwulin on 16/5/22.
- */
 @RestController
-public class OrderController extends BaseController{
-    OrderService orderService;
-    //1111
+@Api("订单")
+@RequestMapping(value = "plus")
+public class OrderController extends BaseController {
+	private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
+	@Autowired
+	private OrderService orderService;
+	
+	
 }

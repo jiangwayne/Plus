fae3f4e9a51241ad9823819b5f7f15fc9bc4129e
jiangwei <jiangwei@luk360.com>
2016-09-18 17:49:46 +0800

    update

--- src/main/java/com/plus/server/filter/SessionFilter.java
+++ src/main/java/com/plus/server/filter/SessionFilter.java
@@ -23,7 +23,7 @@ public class SessionFilter implements Filter {
 	}
 	
 	private static final String[] openUrlArr = new String[] { "login", "getValidateCode", "getEmailValidateCode",
-			"register", "ftl/","static/","file/","/swagger","/picLibList"};
+			"register", "ftl/","static/","file/","/swagger","/picLibList","/listHomePageProduct"};
 
 	private boolean isOpenUrl(String url) {
 		for (String openUrl : openUrlArr) {
